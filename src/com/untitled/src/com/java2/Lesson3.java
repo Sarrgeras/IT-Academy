a2cde642a7642d409390d22a0b655098d63bd114
ddd015 <dmirty015@yandex.ru>
2022-10-21 19:31:30 +0300

    лекция3

--- src/com/untitled/src/com/java2/Lesson3.java
+++ src/com/untitled/src/com/java2/Lesson3.java
@@ -3,6 +3,21 @@ package com.java2;
 public class Lesson3 {
     public static void main(String[]args){
         System.out.println("hello");
+        // операторы инкр и дискр.
+        int num = 5;
+        System.out.println(++num*2);
+        int num2 = 5;
+        System.out.println(num2++*2);
+        //
+        int z = 5;
+         z %= 2;
+        System.out.println(z);
+        boolean isTrue = z!=5;
+        System.out.println(isTrue);
+        // Приведение типов (!Почитать про бинарный сдвиг)
+
+
+
 
 
 
